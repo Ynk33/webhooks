8d00f90685b3e967ed0b7dee5ead620054484499
Yannick Tirand <yannick.tirand@gravitygoat.com>
2024-04-07 09:51:19 +1200

    Sets up a basic webhooks to check payload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+
+const port = 80;
+
+const payloadExample = {
+  "action": "opened",
+  "issue": {
+    "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+    "number": 1347
+  },
+  "repository" : {
+    "id": 1296269,
+    "full_name": "octocat/Hello-World",
+    "owner": {
+      "login": "octocat",
+      "id": 1
+    }
+  },
+  "sender": {
+    "login": "octocat",
+    "id": 1
+  }
+ }
+
+ const developBranch = "develop"
+ const mainBranch = "main"
+
+ const app = express()
+
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+
+  // TODO
+  // - Parse the body
+  let data = body.data;
+  console.log(data);
+
+  // - Find out branch has been pushed
+  // --- If it is not main or develop, ignore and do nothing
+  // - Find out which project is concerned
+  // - Launch the appropriate script according to the information above
+  // --- deploy.sh if it is on main
+  // --- deploy-preprod.sh if it is on develop
+
+  res.send("Received!")
+})
+
+app.listen(port, () => {
+  console.log(`Webhooks app listening on port ${port}`)
+})
