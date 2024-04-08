a67659dd11dfec9eb9f9758023776962797b12ab
Yannick Tirand <yannick.tirand@gravitygoat.com>
2024-04-08 12:59:10 +1200

    Fixes update.sh script

--- scripts/webhooks/update.sh
+++ scripts/webhooks/update.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 #!/bin/bash
 
-PATH=/var/www/webhooks
-
-echo Connected as $(whoami) at $(pwd)
-
 echo Pulling updates...
-su - yanka -c "cd $PATH && git pull"
+su - yanka -c "cd /var/www/webhooks && git pull"
 
 echo Installing dependencies...
-su - yanka -c "cd $PATH && npm install"
+su - yanka -c "cd /var/www/webhooks && npm install"
 
 echo Building project...
-su - yanka -c "cd $PATH && npm run build"
+su - yanka -c "cd /var/www/webhooks && npm run build"
 
 echo Restart daemon...
- pm2 restart webhooks
+pm2 restart webhooks
 
 echo -e "\033[32mDone!\033[0m Webhooks updated and running."
