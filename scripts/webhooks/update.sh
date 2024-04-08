841fe2b48c6b8956943898fc9188b63b09b83447
Yannick Tirand <yannick.tirand@gravitygoat.com>
2024-04-08 12:31:29 +1200

    Try somth

--- scripts/webhooks/update.sh
+++ scripts/webhooks/update.sh
@@ -13,6 +13,6 @@ echo Building project...
 su - yanka -c "npm run build"
 
 echo Restart daemon...
-pm2 restart webhooks
+# pm2 restart webhooks
 
 echo -e "\033[32mDone!\033[0m Webhooks updated and running."
